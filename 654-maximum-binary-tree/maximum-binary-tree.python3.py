1c7b0b4740c5203da2f4f2b18f43919a2aead5f0
EINDEX <snowstarlbk@gmail.com>
2017-09-26 23:51:50 +0800

    maximum-binary-tree python3 solution

---
+++ 654-maximum-binary-tree/maximum-binary-tree.python3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def constructMaximumBinaryTree(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if nums:
+            max_num = max(nums)
+            i =  nums.index(max_num)
+            tree = TreeNode(max_num)
+            tree.left = self.constructMaximumBinaryTree(nums[:i])
+            if i +1 < len(nums):
+                tree.right = self.constructMaximumBinaryTree(nums[i+1:])
+            return tree
+        
