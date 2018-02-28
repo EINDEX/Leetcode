7357a07e0e7121b5dc9af157212a260fac3d19a9
EINDEX <snowstarlbk@gmail.com>
2018-02-28 18:16:33 +0800

    longest-continuous-increasing-subsequence python3 solution

---
+++ 674-longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.python3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findLengthOfLCIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        l = len(nums)
+        res = 0
+        tmp = nums[0] - 1
+        count = 0
+        for x in range(l):
+            if nums[x] > tmp:
+                count += 1
+                if count > res:
+                    res = count
+            else:
+                count = 1
+            tmp = nums[x]
+        return res
+            
