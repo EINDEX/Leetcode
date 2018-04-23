94c02bd577ed1db8798c4727a576f5c38d1f3ba2
EINDEX <snowstarlbk@gmail.com>
2018-04-23 17:39:34 +0800

    sqrtx python3 solution

---
+++ 69-sqrtx/sqrtx.python3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x == 0:
+            return 0
+        l = 0
+        h = x // 2 + 1
+        mid = 0
+
+        while(l <= h):
+            mid = (l+h) // 2
+            if mid ** 2 > x:
+                h = mid - 1
+            elif mid ** 2 < x:
+                l = mid + 1
+            else:
+                return mid
+        return (l+h) // 2
+
