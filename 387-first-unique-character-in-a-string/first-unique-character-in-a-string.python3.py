49987a93c5ece8526b8164711429c8ba6c4ba24a
EINDEX <snowstarlbk@gmail.com>
2018-05-07 11:35:37 +0800

    first-unique-character-in-a-string python3 solution

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.python3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        bucket = {chr(k):0 for k in range(97,123)}
+        for i, k in enumerate(s):
+            if bucket[k] == 0:
+                bucket[k] = i+1
+            elif bucket[k] > 0:
+                bucket[k] = -1
+       
+        for i, k in enumerate(s):
+            if bucket[k] > 0:
+                return i
+        else:
+            return -1
