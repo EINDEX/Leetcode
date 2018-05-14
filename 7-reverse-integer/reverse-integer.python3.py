db0b4da0950628981382d906504cfecc8397412e
EINDEX <snowstarlbk@gmail.com>
2018-05-15 00:06:18 +0800

    reverse-integer python3 solution

---
+++ 7-reverse-integer/reverse-integer.python3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 0:
+            x = -x
+            res = -int(str(x)[::-1])
+        else:
+            res = int(str(x)[::-1])
+        if res < - 2**31:
+            return 0
+        elif res > 2**31 -1:
+            return 0
+        else:
+            return res
+        
