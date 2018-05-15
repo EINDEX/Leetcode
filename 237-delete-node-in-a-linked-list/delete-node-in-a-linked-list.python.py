13ee3576679c5ee5b2ee3e6a8a40e4172d3c0682
EINDEX <snowstarlbk@gmail.com>
2018-05-15 14:18:18 +0800

    delete-node-in-a-linked-list python solution

---
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.python.py
@@ -0,0 +1,34 @@
+#
+# [237] Delete Node in a Linked List
+#
+# https://leetcode-cn.com/problems/delete-node-in-a-linked-list/description/
+#
+# algorithms
+# Easy (36.55%)
+# Total Accepted:    1.9K
+# Total Submissions: 5.2K
+# Testcase Example:  '[0,1]\nnode at index 0 (node.val = 0)'
+#
+# 请编写一个函数，使其可以删除某个链表中给定的（非末尾的）节点，您将只被给予要求被删除的节点。
+# 
+# 比如：假设该链表为 1 -> 2 -> 3 -> 4  ，给定您的为该链表中值为 3 的第三个节点，那么在调用了您的函数之后，该链表则应变成 1 -> 2
+# -> 4 。
+# 
+#
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        if not node.next:
+            return
+        
+        node.val = node.next.val
+        node.next = node.next.next
