e9a46793306ff6a78327255458756b5f9acf125a
EINDEX <snowstarlbk@gmail.com>
2018-09-06 15:24:27 +0800

    valid-anagram python3 solution

---
+++ 242-valid-anagram/valid-anagram.python3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        d = {}
+        for i in s:
+            d.setdefault(i, 0)
+            d[i] += 1
+        
+        for i in t:
+            if i not in d:
+                return False
+            else:
+                if d[i] <= 0:
+                    return False
+                d[i] -= 1
+        return True
