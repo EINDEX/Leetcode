f6f4d8c38eb79bec27c0363c163fba3833351764
EINDEX <snowstarlbk@gmail.com>
2018-09-06 15:22:47 +0800

    longest-common-prefix python3 solution

---
+++ 14-longest-common-prefix/longest-common-prefix.python3.py
@@ -0,0 +1,31 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        
+        length = len(strs)
+        
+        flag = True
+        res = ""
+        index = 0
+        while flag:
+            temp = ""
+            for x in range(length):
+                if len(strs[x]) > index:
+                    if not temp:
+                        temp = strs[x][index]
+                    else:
+                        if temp != strs[x][index]:
+                            return res
+                else:
+                    return res
+            else:
+                res += temp
+            index += 1
+                    
+                
+        
