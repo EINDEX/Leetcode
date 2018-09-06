7493496bb5e4d0eb1c7d254705621903ff1e4fba
EINDEX <snowstarlbk@gmail.com>
2018-09-06 15:24:53 +0800

    remove-duplicates-from-sorted-array python3 solution

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.python3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        x = nums[0]
+        for n in nums[1:]:
+            if n == x:
+                nums.remove(n)
+            else:
+                x = n
+        return len(nums)
