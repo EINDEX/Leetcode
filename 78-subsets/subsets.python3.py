82149967957f415834f36c1989d7b7191c537d19
EINDEX <snowstarlbk@gmail.com>
2018-10-06 17:26:03 +0800

    subsets python3 solution

---
+++ 78-subsets/subsets.python3.py
@@ -0,0 +1,7 @@
+class Solution:
+    # Iteratively
+    def subsets(self, nums):
+        res = [[]]
+        for num in sorted(nums):
+            res += [item+[num] for item in res]
+        return res
