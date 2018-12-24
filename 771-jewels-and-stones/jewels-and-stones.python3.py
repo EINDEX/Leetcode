88cc721c2ecce47cae6eb23269fee950428895d7
EINDEX <snowstarlbk@gmail.com>
2018-12-24 22:39:48 +0800

    jewels-and-stones python3 solution

---
+++ 771-jewels-and-stones/jewels-and-stones.python3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        return sum([S.count(j) for j in J])
+            
+        
