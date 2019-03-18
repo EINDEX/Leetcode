8bf04c67440113bf123f73d509e8a5261ac1f3ed
EINDEX <snowstarlbk@gmail.com>
2019-03-19 00:37:33 +0800

    add-binary python solution

---
+++ 67-add-binary/add-binary.python.py
@@ -0,0 +1,10 @@
+class Solution:
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        return str(bin(int(a, 2) + int(b, 2))).replace('0b', '')
+
+
