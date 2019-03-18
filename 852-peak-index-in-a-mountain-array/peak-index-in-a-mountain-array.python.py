28dcad04a6c53a55d38aa8eb5a5847ee86d06a63
EINDEX <snowstarlbk@gmail.com>
2019-03-19 00:45:50 +0800

    peak-index-in-a-mountain-array python solution

---
+++ 852-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.python.py
@@ -0,0 +1,9 @@
+class Solution:
+    def peakIndexInMountainArray(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        for i in range(len(A)):
+            if A[i] > A[i+1]:
+                return i
