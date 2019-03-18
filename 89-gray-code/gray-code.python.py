f4ba397f11df9376dbe864f1f29dbbe27f5cd9a1
EINDEX <snowstarlbk@gmail.com>
2019-03-19 00:55:13 +0800

    gray-code python solution

---
+++ 89-gray-code/gray-code.python.py
@@ -0,0 +1,8 @@
+class Solution:
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        return [i ^ (i >> 1) for i in range(1 << n)]
+        
