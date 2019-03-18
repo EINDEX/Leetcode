457907ef0f43cf62e88d5984a7c5a2e204751590
EINDEX <snowstarlbk@gmail.com>
2019-03-18 23:53:04 +0800

    same-tree python solution

---
+++ 100-same-tree/same-tree.python.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        res = False
+        if (p is None and q is not None) or (p is not None and q is None):
+            return False
+        if p is None and q is None:
+            return True
+        if p is not None and q is not None:
+            if p.val == q.val:
+                res = True
+        return res and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+
+
